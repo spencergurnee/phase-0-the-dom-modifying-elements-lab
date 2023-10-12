cf056cd58fcb4622d049d2ed41752cf2580c6d3b
Spencer Gurnee <spencergurnee@gmail.com>
2023-10-11 21:56:23 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+const newHeader = document.querySelector("h1");
+if (newHeader) {
+  console.log(newHeader.nodeName); // Accessing nodeName property
+} else {
+  console.log("No h1 element found");
+}
